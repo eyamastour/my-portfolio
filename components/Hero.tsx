24fa23c740ead97a1db1662cf81203d6e3942679
eyamastour <eyamastour22@gmail.com>
2024-08-20 15:25:58 +0200

    added grid,globe

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,14 +26,13 @@ const Hero = () => {
             <div className='flex justify-center relative my-20 z-10'>
                 <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                     <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
-                        Dynamic Web  with Next.js
                     </h2>
                     <TextGenerateEffect
                      className='text-center text-[40px] md:text-5xl lg:text-6xl'
-                     words='Turning Ideas into Seamless User Experiences'
+                     words='Turning Ideas into Elegant Web Solutions'
                     />
                     <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-                        Hi, I&rsquo;m Eya Mastour, a Front End Developer.
+                        Hi! I&rsquo;m Eya Mastour, a  Web Developer.
                     </p>
                     <a href="#about">
                         <MagicButton 
