8f28306459408c086a387d5a9e6432fc82c41ced
eyamastour <eyamastour22@gmail.com>
2024-08-28 17:09:27 +0200

    last commit

--- data/index.tsx
+++ data/index.tsx
@@ -49,8 +49,8 @@ export const navItems = [
   
     {
       id: 5,
-      title: "Currently ",
-      description: "...",
+      title: "Currently building  an app for front-end developers to share and support each other.",
+      description: "Junio Coders",
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
       titleClassName: "justify-center md:justify-start lg:justify-center",
