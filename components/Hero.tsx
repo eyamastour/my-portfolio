7ed7f1636c3c88bf89152d9bc5b8531f3c9f4176
eyamastour <eyamastour22@gmail.com>
2024-08-29 17:34:40 +0200

    Last updates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -36,7 +36,7 @@ const Hero = () => {
                     <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
                         Hi! I&rsquo;m Eya Mastour, a  Web Developer.
                     </p>
-                    <a href="/CV_EYA_MASTOUR.pdf" download="Eya-Mastour-CV.pdf">
+                    <a href="/Resume-EYA-MASTOUR.pdf" download="Eya-Mastour-CV.pdf">
                         <MagicButton 
                         title="Get My Resume "
                         icon={<FaDownload /> }
